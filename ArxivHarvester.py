cd5e6954718ca207a7c12555ec64463bab369387
RuslanBrilenkov <ruslan.brilenkov@gmail.com>
2020-05-29 00:58:18 +0200

    Greately improved the harvester. Added the filtering for paper link, not only interesting tile. Also, automatic saving with the unique name.

--- ArxivHarvester.py
+++ ArxivHarvester.py
@@ -1,6 +1,8 @@
 import urllib.request
 import re
 import numpy as np
+import datetime
+import pandas as pd
 
 def Harvester():
 	'''
@@ -27,7 +29,7 @@ def Harvester():
 		print(e)
 	return None
 
-def ReadingArxivData():
+def SavingTitles():
 	'''
 	Reading saved (.txt) data of website response.
 	Separating the titles from the text using regular expressions.
@@ -73,8 +75,68 @@ def ReadingArxivData():
 		pass
 	
 	return None
+
+def SavingLinksAndTitles():
+	'''
+	This code read the Arxiv response and filters out infor about 
+	paper title and the link to .pdf
+	In return, saves a .txt file with this information.
+	And prints out how many interesting papers found.
 	
+	Saved file as a name: 
+	current daytime + number of papers found
+	'''
+	try:
+		with open("Arxiv_data.txt", "r") as file:
+			Data = file.read()
+		
+		# separating Links to papers and their titles
+		LinkPlusTitle = re.findall(r"\<a href=\"\/abs\/(.*?)\".*?Title:</span> (.*?)\\n</div>", Data)
+		
+		# Example key words of our interest
+		My_keyWords = ['ALMA', 'dust', 'attenuation','IRX-beta', 'IRX', 'HST', '21cm']
+		
+		title_counter = 0
+		TitlesToSave = []
+		LinksToSave = []
+		for text in LinkPlusTitle:
+			# we split the words
+			titleList = text[1].split(' ')
+			
+			# Checking the intersection between array of our key words and the selected titles
+			# (in case there are more than 1 key word in the title, get the tite once)
+			if (len(np.intersect1d(My_keyWords, titleList))>0):
+				# print title names if you wish:
+				#print("Found {keyword} in\n{title}\n{link}\n".format(keyword=np.intersect1d(My_keyWords, titleList), title = text[1], link = "https://arxiv.org/pdf/"+text[0]+".pdf"))
+				TitlesToSave.append(str(text[1]))
+				LinksToSave.append(str("https://arxiv.org/pdf/"+text[0]+".pdf"))
+				title_counter += 1
+				
+		
+		extention = ".txt"
+		currentDT = datetime.datetime.now()
+		timeStr = str(currentDT.year)+"_"+str(currentDT.month)+"_"+str(currentDT.day)
+		filenameToSave = timeStr+"_with_"+str(title_counter)+"_papers"+extention
+		with open(filenameToSave, "w") as file:
+			file.write(str(TitlesToSave) )
+			file.write(str(LinksToSave) )
+		
+		# printing a nice message for a user with a number of articles found
+		if (title_counter==0):
+			print("Unfortunately, I did not find any articles! Try to change the key words.")
+		else:
+			print("\nFound {} titles you might be interested in.\n".format(title_counter))
+	except Exception as e:
+		print(e)
+	
+	return None
+
+def UsingPands():
+	pass
+
 if (__name__=="__main__"):
 	#Harvester()
-	ReadingArxivData()
+	#SavingTitles()
+	SavingLinksAndTitles()
+	#PrintData()
 
